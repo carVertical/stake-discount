58554eed8cb8b4e8aca7b852006af4166aa676f2
Justin Zaliaduonis <justinas.zaliaduonis@gmail.com>
2018-11-15 18:46:49 -0800

    fix discount setting and discount report

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -3,24 +3,29 @@ pragma solidity ^0.4.24;
 import "./ERC20.sol";
 import "./IERC20.sol";
 import "./Ownable.sol";
-import "./SafeERC20.sol";
 import "./SafeMath.sol";
 
+//Set max and min discount and make it increase monotonically
 contract Staking is Ownable{
 
 IERC20 public cVToken;
-uint256[10] public discounts;
-uint256[10] public discountThreshold;
 uint256 public stakingTime;
+uint256 public minThreshold;
+uint256 public maxThreshold;
+uint256 [10]discounts;
 
   using SafeMath for uint256;
-  using SafeERC20 for IERC20;
 
 constructor(
-  IERC20 _cVToken, uint256 _stakingTime)public{
+  IERC20 _cVToken,
+  uint256 _stakingTime,
+  uint256 _minThreshold,
+  uint256 _maxThreshold)public{
     require(_cVToken != address(0));
     cVToken = _cVToken;
     stakingTime = _stakingTime;
+    minThreshold = _minThreshold; //in wei
+    maxThreshold = _maxThreshold; //in wei
   }
 
 struct StakeInfo{
@@ -37,80 +42,74 @@ function stakeOf(address staker)public view returns(uint256){
   return StakeTable[staker].stakeAmount;
 }
 
-function discountOf(address staker)public view returns(uint256){
+function discountOf(address staker)public view returns(uint256){ //Change and rewrite with formulas
 
-  uint256 amount = stakeOf(staker);
+  uint256 stakedAmount = stakeOf(staker);
 
-  if(amount <  discountThreshold[0]){
-    return 0;
-  }
-  else if(amount >=discountThreshold[0] && amount<discountThreshold[1]){
-    return discounts[0];
-  }
-  else if(amount >= discountThreshold[1] && amount <discountThreshold[2]){
-    return discounts[1];
-  }
-  else if(amount >= discountThreshold[2] && amount <discountThreshold[3]){
-    return discounts[2];
-  }
-  else if(amount >= discountThreshold[3] && amount <discountThreshold[4]){
-    return discounts[3];
-  }
-  else if(amount >= discountThreshold[4] && amount <discountThreshold[5]){
-    return discounts[4];
-  }
-  else if(amount >= discountThreshold[5] && amount <discountThreshold[6]){
-    return discounts[5];
-  }
-  else if(amount >= discountThreshold[6] && amount <discountThreshold[7]){
-    return discounts[6];
-  }
-  else if(amount >= discountThreshold[7] && amount <discountThreshold[8]){
-    return discounts[7];
-  }
-  else if(amount >= discountThreshold[8] && amount <discountThreshold[9]){
-    return discounts[8];
-  }
-  else {
-    return discounts[9];
-  }
+  if(stakedAmount >= maxThreshold) return discounts[0];
+  if(stakedAmount < minThreshold) return 0;
 
+  uint256 TEN = 10;
+  uint256 numerator = stakedAmount.sub(minThreshold);
+  uint256 denominator = maxThreshold.sub(minThreshold);
+
+  uint256 discountIndex = uint256(TEN.mul(uint256(numerator.div(denominator))));
+
+  return discounts[discountIndex];
 }
 
-function stake(address staker, uint256 amount) onlyOwner{
+function stake(uint256 amount){
+
+  address staker = msg.sender;
+
+  require(cVToken.transferFrom(staker,address(this),amount)); //Transfer in wei
 
   if(StakeTable[staker].stakeAmount == 0){
   StakeTable[staker].unstakeTime = stakingTime.add(now);
-  StakeTable[staker].stakeAmount = amount;
+  StakeTable[staker].stakeAmount = amount; // in wei
   }
   else{ //FFR discuss second staking
     StakeTable[staker].stakeAmount = StakeTable[staker].stakeAmount.add(amount);
   }
 }
 
-function unstake(address staker) onlyOwner{
+function unstake(){
+address staker = msg.sender;
 
 require(StakeTable[staker].unstakeTime <= now);
 require(StakeTable[staker].stakeAmount > 0);
 
-cVToken.safeTransfer(staker,StakeTable[staker].stakeAmount);
+cVToken.transfer(staker,StakeTable[staker].stakeAmount);
 
 StakeTable[staker].stakeAmount = 0;
 StakeTable[staker].unstakeTime = 0;
 }
 
-function setDiscount(uint256 _discountNr,uint256 _newDiscount)onlyOwner{
-  require(_discountNr >= 1 && _discountNr <=10 );
-  require(_newDiscount >=0 && _newDiscount<=100);
-  _discountNr = _discountNr.sub(1);
-  discounts[_discountNr] = _newDiscount;
+function setDiscounts(uint256 _minDiscount,uint256 _maxDiscount)onlyOwner{
+  require(_minDiscount > 0);
+  require(_maxDiscount < 100);
+  require(_minDiscount < _maxDiscount);
+
+  uint256 interval = _maxDiscount.sub(_minDiscount);
+
+  uint256 period = interval.div(9);
+
+  discounts[0] = _minDiscount;
+
+  for(uint256 e=1; e<10; e++){
+    discounts[e] = discounts[e-1].add(period);
+  }
 }
 
-function setThreshold(uint256 _thresholdNr,uint256 _newThreshold)public onlyOwner{
-  require(_thresholdNr>= 1 && _thresholdNr<= 10);
+function setThresholds(uint256 _minThreshold ,uint256 _maxThreshold)public onlyOwner{
+  require(_minThreshold < _maxThreshold);
 
-  _thresholdNr = _thresholdNr.sub(1);
-  discountThreshold[_thresholdNr] = _newThreshold;
+  minThreshold = _minThreshold;
+  maxThreshold = _maxThreshold;
+}
+
+function setStakingTime(uint256 _stakingTime) onlyOwner{
+  stakingTime = _stakingTime;
 }
 
 function getAddress()returns(address){

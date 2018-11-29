80b47798bc0d4862c223420d8bdc29d066edc982
Justin Zaliaduonis <justinas.zaliaduonis@gmail.com>
2018-11-29 13:34:06 +0200

    Upgrade tests

--- migrations/2_stake_migration.js
+++ migrations/2_stake_migration.js
@@ -4,10 +4,12 @@ var StakeContract = artifacts.require("./Staking.sol");
 var tokenAddressRinkeby = "0xAfff042F602762B59442660aCDF34fdE8681D016";
 var tokenAddressMain = "0xdA6cb58A0D0C01610a29c5A65c303e13e885887C";
 
+const discounts = [2,4,6,8,10,12,14,16,18,20];
+
 module.exports = function(deployer) {
   if (deployer.network == 'development') {
     return deployer.deploy(CvToken).then(() => {
-      return deployer.deploy(StakeContract,CvToken.address,100,100,200);
+      return deployer.deploy(StakeContract,CvToken.address,100,100,3000, discounts);
     });
   } else if(deployer.network == 'rinkeby') {
     return deployer.deploy(StakeContract,tokenAddressRinkeby);
